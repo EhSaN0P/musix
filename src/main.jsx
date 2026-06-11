d1c2b48091e7e0badd31fdc146d48582255deff1
EhSaN <ehsanmg13@gmail.com>
2026-06-11 18:11:30 +0330

    deploying

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,15 @@ import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
 import { store } from "./store/store.js";
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from './services/queryClient';
 createRoot(document.getElementById('root')).render(
     <QueryClientProvider client={queryClient}>
         <Provider store={store}>
-                <BrowserRouter>
+                <HashRouter>
                     <App />
-                </BrowserRouter>
+                </HashRouter>
             </Provider>
     </QueryClientProvider>
 )
